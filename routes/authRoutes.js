c80e81110b44281727fdb974d4bc874ff25612f8
MohammadShakaroun <103925160+MohammadShakaroun@users.noreply.github.com>
2025-05-21 13:46:53 +0300

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { register, login, getProfile } = require('../controllers/authController');
+const { register, login, getProfile } = require('../controllers/authControllers');
 const authMiddleware = require('../middleware/authMiddleware');
 
 router.post('/register', register);
